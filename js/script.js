2e3261546936f7bf4db4d83c58042c9cc4ea3e52
ogapants <ogapamyu@gmail.com>
2022-02-08 00:27:12 +0900

    use location.replace

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
 window.onload = function() {
-  var link = document.querySelector(".app-bar-link")
-  link.href = document.URL.replace("/u/0/","/u/1/")
-  link.textContent =  "Switch To /u/1/"
-  link.removeAttribute("target")
-  link.removeAttribute("rel")
-  // <a _ngcontent-elu-c157="" class="app-bar-link" href="/u/0/" target="_blank" rel="noopener">ドキュメントに移動</a>
-  // ->
-  // <a _ngcontent-byp-c157="" class="app-bar-link" href="/u/1/">Switch To /u/1/</a>
+  window.location.replace(document.URL.replace("/u/0/","/u/1/"))
 }
