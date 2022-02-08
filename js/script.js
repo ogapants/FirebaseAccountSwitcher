9cd58391b16738b870c51cc62cc5bfaf7c75c2bf
ogapan <ogapamyu@gmail.com>
2022-02-08 17:11:46 +0900

    format

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-window.onload = function() {
-  window.location.replace(document.URL.replace("/u/0/","/u/1/"))
+window.onload = function () {
+  window.location.replace(document.URL.replace("/u/0/", "/u/1/"))
 }
