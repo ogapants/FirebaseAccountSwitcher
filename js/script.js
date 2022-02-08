990132c019063370638d01d390b18f8f3d9f5b5a
ogapan <ogapamyu@gmail.com>
2022-02-08 18:10:12 +0900

    support google drive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.onload = function () {
-  if (document.URL.startsWith("https://console.firebase.google.com/u/0/")) {
+  if (document.URL.includes("/u/0/")) {
     window.location.replace(document.URL.replace("/u/0/", "/u/1/"))
+  } else if (document.URL.startsWith("https://drive.google.com/drive/folders/")) {
+    window.location.replace(document.URL.replace("/drive/folders/", "/drive/u/1/folders/"))
   }
 }
