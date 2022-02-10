edccd0dbada12ccabf9db5124179a4eb1ff01b61
ogapants <ogapamyu@gmail.com>
2022-02-10 22:58:41 +0900

    use Template literals for account num

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 window.onload = function () {
+  const replaceAccountNum = 1
   if (document.URL.includes("/u/0/")) {
-    window.location.replace(document.URL.replace("/u/0/", "/u/1/"))
+    window.location.replace(document.URL.replace("/u/0/", `/u/${replaceAccountNum}/`))
   } else if (document.URL.startsWith("https://drive.google.com/drive/folders/")) {
-    window.location.replace(document.URL.replace("/drive/folders/", "/drive/u/1/folders/"))
+    window.location.replace(document.URL.replace("/drive/folders/", `/drive/u/${replaceAccountNum}/folders/`))
   }
 }
