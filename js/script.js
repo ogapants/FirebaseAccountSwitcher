462ce70470626a3db2e1da2e8f46e525c263df44
ogapan <ogapamyu@gmail.com>
2022-02-12 00:45:29 +0900

    add copy func

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
+const replaceAccountNum = 1
+
 window.onload = function () {
-  const replaceAccountNum = 1
   if (document.URL.includes("/u/0/")) {
     window.location.replace(document.URL.replace("/u/0/", `/u/${replaceAccountNum}/`))
   } else if (document.URL.startsWith("https://drive.google.com/drive/folders/")) {
     window.location.replace(document.URL.replace("/drive/folders/", `/drive/u/${replaceAccountNum}/folders/`))
   }
+
+  //Receive event from background.js 
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("received: " + sender)
+    var newUrl = document.URL.replace(`/u/${replaceAccountNum}/`, "/u/0/")
+    navigator.clipboard.writeText(newUrl)
+    sendResponse({})
+  })
 }
