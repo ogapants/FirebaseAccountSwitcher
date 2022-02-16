711750076fe6428d70563aa8d69e3381ae5c212f
ogapan <ogapamyu@gmail.com>
2022-02-16 17:32:05 +0900

    support google meet

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-const replaceAccountNum = 1
+const replaceAccountNum = String(1)
 
 window.onload = function () {
   var url = new URL(window.location.href)
 
   if (url.href.includes("/u/0/")) {
     window.location.replace(url.href.replace("/u/0/", `/u/${replaceAccountNum}/`))
+
   } else if (url.href.startsWith("https://drive.google.com/drive/folders/")) {
     window.location.replace(url.href.replace("/drive/folders/", `/drive/u/${replaceAccountNum}/folders/`))
+
+  } else if (url.hostname == "meet.google.com" && url.searchParams.get("authuser") !== replaceAccountNum) {
+    url.searchParams.set("authuser", replaceAccountNum)
+    window.location.replace(url)
   }
 
   //Receive event from background.js 
