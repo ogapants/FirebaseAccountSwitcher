f72a40ddd66b2a07ee85055502f9bb55f7ae6dff
ogapan <ogapamyu@gmail.com>
2022-02-16 16:50:37 +0900

    Use location.href instead of document.URL

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const replaceAccountNum = 1
 
 window.onload = function () {
-  if (document.URL.includes("/u/0/")) {
-    window.location.replace(document.URL.replace("/u/0/", `/u/${replaceAccountNum}/`))
-  } else if (document.URL.startsWith("https://drive.google.com/drive/folders/")) {
-    window.location.replace(document.URL.replace("/drive/folders/", `/drive/u/${replaceAccountNum}/folders/`))
+  var url = new URL(window.location.href)
+
+  if (url.href.includes("/u/0/")) {
+    window.location.replace(url.href.replace("/u/0/", `/u/${replaceAccountNum}/`))
+  } else if (url.href.startsWith("https://drive.google.com/drive/folders/")) {
+    window.location.replace(url.href.replace("/drive/folders/", `/drive/u/${replaceAccountNum}/folders/`))
   }
 
   //Receive event from background.js 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("received: " + sender)
-    var newUrl = document.URL.replace(`/u/${replaceAccountNum}/`, "/u/0/")
+    var newUrl = window.location.href.replace(`/u/${replaceAccountNum}/`, "/u/0/")
     navigator.clipboard.writeText(newUrl)
     sendResponse({})
   })
