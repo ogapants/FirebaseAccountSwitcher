6e40e801d3346394a0537e2ad836bfaea4c258c0
ogapants <ogapamyu@gmail.com>
2022-02-20 00:42:06 +0900

    format with Prettier

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.contextMenus.create({
-  "id": "copy_url",
-  "title": "Copy URL as /u/0/",
-  "type": "normal",
-  "contexts": ["all"],
-})
+  id: "copy_url",
+  title: "Copy URL as /u/0/",
+  type: "normal",
+  contexts: ["all"],
+});
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if(info.menuItemId == "copy_url") {
+  if (info.menuItemId == "copy_url") {
     chrome.tabs.sendMessage(tab.id, null, null);
   }
-})
+});
